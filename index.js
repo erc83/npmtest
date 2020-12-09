897137555658a5143cc64bc9bea1e5a12b4b8b23
erc83 <espinoza.eric@gmail.com>
2020-12-09 01:26:12 -0300

    index

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const _ = require('underscore');
 const numbers = [1,7,8,9,8,7];
 console.log(_.min(numbers));   // con esto llama el minimo
 console.log(_.max(numbers));   // con esto llama el maximo
+
+
